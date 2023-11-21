fe4dd820b9275c950f7d0d6d299617574d1b50eb
version1 <kt7642@g.rit.edu>
2023-11-21 10:45:56 +0400

    second commit

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'swen-261-600';
+
+  isMenuOpened: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  clickedOutside(): void {
+    this.isMenuOpened = false;
+  }
+}
