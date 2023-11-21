00413c1e6f19f6158f14900b167faf5030e85f21
version1 <kt7642@g.rit.edu>
2023-11-21 10:46:19 +0400

    second commit

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
