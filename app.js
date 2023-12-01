b4e520dcf2f7d4794544e91b08d67b1caf14761c
Humais-Javed <112623717+Humais-Javed@users.noreply.github.com>
2023-12-01 12:38:35 +0400

    updated the code so now it verifies user existence and directs them to a placeholder homepage

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/Project', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/Project');
 
 // Create a user schema
 const userSchema = new mongoose.Schema({
@@ -19,6 +18,11 @@ const User = mongoose.model('User', userSchema);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key', // Change this to a random string for security
+  resave: false,
+  saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -28,22 +32,36 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      // Query the database to find a user with the provided credentials
-      const user = await User.findOne({ username, password });
-  
-      if (user) {
-        res.send('<script>alert("Correct"); window.location.href="/";</script>');
-      } else {
-        res.send('<script>alert("Incorrect"); window.location.href="/";</script>');
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('<script>alert("Error"); window.location.href="/";</script>');
+  const { username, password } = req.body;
+
+  try {
+    // Query the database to find a user with the provided credentials
+    const user = await User.findOne({ username, password });
+
+    if (user) {
+      // Store user information in the session
+      req.session.userId = user._id;
+      res.redirect('/curation'); // Redirect to the page
+    } else {
+      res.render('login', { error: 'Incorrect username or password' }); // Render error message
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).render('login', { error: 'An error occurred' });
+  }
+});
+
+// New route for curation page
+app.get('/curation', (req, res) => {
+  // Check if the user is authenticated (session contains user information)
+  if (req.session.userId) {
+    // Render the cooking information page
+    res.render('curation');
+  } else {
+    // Redirect to login if not authenticated
+    res.redirect('/');
+  }
+});
 
 // Start server
 const port = process.env.PORT || 3000;
